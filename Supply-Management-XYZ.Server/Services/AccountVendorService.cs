eb80cadfac0b00cc2e9e06afae2e7b422fb75817
putuagra <iputuagra@gmail.com>
2023-12-29 21:39:42 +0700

    feat: create account vendor service

---
+++ Supply-Management-XYZ.Server/Services/AccountVendorService.cs
@@ -0,0 +1,100 @@
+﻿using Supply_Management_XYZ.Server.Contracts;
+using Supply_Management_XYZ.Server.Data;
+using Supply_Management_XYZ.Server.DataTransferObjects.AccountVendors;
+using Supply_Management_XYZ.Server.Models;
+using Supply_Management_XYZ.Server.Utilities.Handlers;
+
+namespace Supply_Management_XYZ.Server.Services;
+
+public class AccountVendorService
+{
+    private readonly SupplyManagementDbContext _supplyManagementDbContext;
+    private readonly IAccountVendorRepository _accountVendorRepository;
+    private readonly IVendorRepository _vendorRepository;
+
+    public AccountVendorService(IAccountVendorRepository accountVendorRepository, SupplyManagementDbContext supplyManagementDbContext, IVendorRepository vendorRepository)
+    {
+        _accountVendorRepository = accountVendorRepository;
+        _supplyManagementDbContext = supplyManagementDbContext;
+        _vendorRepository = vendorRepository;
+    }
+
+    public IEnumerable<AccountVendorDtoGet> Get()
+    {
+        var accountVendors = _accountVendorRepository.GetAll().ToList();
+        if (!accountVendors.Any()) return Enumerable.Empty<AccountVendorDtoGet>();
+        List<AccountVendorDtoGet> accountVendorDtoGets = new List<AccountVendorDtoGet>();
+        foreach (var accountVendor in accountVendors)
+        {
+            accountVendorDtoGets.Add((AccountVendorDtoGet)accountVendor);
+        }
+        return accountVendorDtoGets;
+    }
+
+    public AccountVendorDtoGet? Get(Guid guid)
+    {
+        var accountVendor = _accountVendorRepository.GetByGuid(guid);
+        if (accountVendor is null) return null;
+        return (AccountVendorDtoGet)accountVendor;
+    }
+
+    public AccountVendorDtoCreate? Create(AccountVendorDtoCreate accountVendorsDtoCreate)
+    {
+        var accountVendorCreated = _accountVendorRepository.Create(accountVendorsDtoCreate);
+        if (accountVendorCreated is null) return null;
+        return (AccountVendorDtoCreate)accountVendorCreated;
+    }
+
+    public int Update(AccountVendorDtoUpdate accountVendorDtoUpdate)
+    {
+        var accountVendor = _accountVendorRepository.GetByGuid(accountVendorDtoUpdate.Guid);
+        if (accountVendor is null) return -1;
+        var accountVendorUpdated = _accountVendorRepository.Update(accountVendorDtoUpdate);
+        return accountVendorUpdated ? 1 : 0;
+    }
+
+    public int Delete(Guid guid)
+    {
+        var accountVendor = _accountVendorRepository.GetByGuid(guid);
+        if (accountVendor is null) return -1;
+        var accountVendorDeleted = _accountVendorRepository.Delete(accountVendor);
+        return accountVendorDeleted ? 1 : 0;
+    }
+
+    public bool RegisterVendor(AccountVendorDtoRegister accountVendorDtoRegister)
+    {
+        using var transaction = _supplyManagementDbContext.Database.BeginTransaction();
+
+        try
+        {
+            var vendor = new Vendor
+            {
+                Guid = new Guid(),
+                Name = accountVendorDtoRegister.Name,
+                Email = accountVendorDtoRegister.Email,
+                PhoneNumber = accountVendorDtoRegister.PhoneNumber,
+                PhotoProfile = accountVendorDtoRegister.PhotoProfile,
+                Sector = "a",
+                Type = "a",
+                IsAdminApprove = false,
+                IsManagerApprove = false,
+            };
+            _vendorRepository.Create(vendor);
+
+            var accountVendor = new AccountVendor
+            {
+                Guid = vendor.Guid,
+                Password = HashingHandler.Hash(accountVendorDtoRegister.Password),
+            };
+            _accountVendorRepository.Create(accountVendor);
+
+            transaction.Commit();
+            return true;
+        }
+        catch
+        {
+            transaction.Rollback();
+            return false;
+        }
+    }
+}
