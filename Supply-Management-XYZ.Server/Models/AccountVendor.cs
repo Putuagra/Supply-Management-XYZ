65762235b0d12d74bf9ffe1e5307097f3433447e
putuagra <iputuagra@gmail.com>
2023-12-29 21:38:38 +0700

    feat: add account vendor model

---
+++ Supply-Management-XYZ.Server/Models/AccountVendor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Supply_Management_XYZ.Server.Models;
+
+[Table("tb_m_account_vendors")]
+public class AccountVendor : BaseEntity
+{
+    [Column("password", TypeName = "nvarchar(255)")]
+    public string Password { get; set; }
+
+    // Cardinality
+    public Vendor? Vendor { get; set; }
+}
