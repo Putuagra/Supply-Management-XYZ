47f43c6191a514e735495853c8f20f2a64ef2659
putuagra <iputuagra@gmail.com>
2023-12-29 21:39:30 +0700

    feat: create account vendor repository

---
+++ Supply-Management-XYZ.Server/Repositories/AccountVendorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Supply_Management_XYZ.Server.Contracts;
+using Supply_Management_XYZ.Server.Data;
+using Supply_Management_XYZ.Server.Models;
+
+namespace Supply_Management_XYZ.Server.Repositories
+{
+    public class AccountVendorRepository : GeneralRepository<AccountVendor>, IAccountVendorRepository
+    {
+        public AccountVendorRepository(SupplyManagementDbContext context) : base(context)
+        {
+        }
+    }
+}
