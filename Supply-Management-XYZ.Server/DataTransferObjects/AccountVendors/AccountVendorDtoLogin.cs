892c9a36229ff1169a66baa1ff974caae6ec0913
putuagra <iputuagra@gmail.com>
2023-12-30 11:55:51 +0700

    feat: add dto login

---
+++ Supply-Management-XYZ.Server/DataTransferObjects/AccountVendors/AccountVendorDtoLogin.cs
@@ -0,0 +1,7 @@
+﻿namespace Supply_Management_XYZ.Server.DataTransferObjects.AccountVendors;
+
+public class AccountVendorDtoLogin
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ Supply-Management-XYZ.Server/DataTransferObjects/Accounts/AccountDtoLogin.cs
@@ -0,0 +1,7 @@
+﻿namespace Supply_Management_XYZ.Server.DataTransferObjects.Accounts;
+
+public class AccountDtoLogin
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
