a2718d89e841bd8ef347ba80a210660cab685f01
putuagra <iputuagra@gmail.com>
2023-12-30 11:56:54 +0700

    feat: create get vendor by email repository

--- Supply-Management-XYZ.Server/Repositories/VendorRepository.cs
+++ Supply-Management-XYZ.Server/Repositories/VendorRepository.cs
@@ -9,4 +9,9 @@ public class VendorRepository : GeneralRepository<Vendor>, IVendorRepository
     public VendorRepository(SupplyManagementDbContext context) : base(context)
     {
     }
+
+    public Vendor? GetVendorByEmail(string email)
+    {
+        return Context.Set<Vendor>().FirstOrDefault(v => v.Email == email);
+    }
 }
